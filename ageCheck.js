17d8dec7c653f02389224d2882aac30818e5e013
roxeannechico <roseannechico22@gmail.com>
2024-10-09 23:05:49 +0800

    initial commit

---
+++ ageCheck.js
@@ -0,0 +1,11 @@
+function ageCheck(){
+  
+  let age = prompt("Enter your age:")
+if (age <= 13){
+  alert("You are a child.");
+}else if (age <= 19){
+  alert ("You are a teenager."); 
+}else {
+  alert("You are an adult");
+}
+}
